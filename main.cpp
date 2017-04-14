08f067fa77fb75abff1070ffd089a370017476e5
Brej <adbrej@gmail.com>
2017-04-14 17:08:01 +0200

    po zmianach an referencje

--- main.cpp
+++ main.cpp
@@ -39,32 +39,26 @@ Matrix createMatrix(int sizeRow, int sizeCol, double inValue = 0){
 }
 
 
-//do wywalenia, przejsc na funkcje z Matrix
-template <typename T>
-void displayMatrixT(int size, T **M){
-    for (int row = 0; row < size; ++row) {
-        std::cout << std::setw(2) << std::right << row << " | ";
-        for (int col = 0; col < size; ++col) {
-            std::cout << std::setw(2) << std::right << M[row][col] << " ";
-        }
-        std::cout << std::endl;
-    }
-}
 
-void displayMatrix(Matrix M){
-    for (int row = 0; row < M.sizeRow; ++row) {
+
+void displayMatrix(Matrix &M){
+    int sizeRow = M.sizeRow;
+    int sizeCol = M.sizeCol;
+    for (int row = 0; row < sizeRow; ++row) {
         std::cout << std::setw(2) << std::right << row << " | ";
-        for (int col = 0; col < M.sizeCol; ++col) {
+        for (int col = 0; col < sizeCol; ++col) {
             std::cout << std::setw(2) << std::right << M.Matrix[row][col] << " ";
         }
         std::cout << std::endl;
     }
 }
 
-Matrix addMatrixNxN(Matrix M, Matrix M2, int modyfikator = 1){
-    Matrix out = createMatrix(M.sizeRow, M.sizeCol);
-    for (int row = 0; row < M.sizeRow; ++row) {
-        for (int col = 0; col < M.sizeCol; ++col) {
+Matrix addMatrixNxN(Matrix &M, Matrix &M2, int modyfikator = 1){
+    int sizeRow = M.sizeRow;
+    int sizeCol = M.sizeCol;
+    Matrix out = createMatrix(sizeRow, sizeCol);
+    for (int row = 0; row < sizeRow; ++row) {
+        for (int col = 0; col < sizeCol; ++col) {
             out.Matrix[row][col] = M.Matrix[row][col] + (M2.Matrix[row][col] * modyfikator);
         }
     }
@@ -74,33 +68,17 @@ Matrix addMatrixNxN(Matrix M, Matrix M2, int modyfikator = 1){
 
 
 
-//przejsc na Matrix wywalic potem
-template <typename T>
-T** DMatrixT(int size, T **M){
-    T** D = 0;
-    D = new T*[size];
-    for (int h = 0; h < size; h++)
-    {
-        D[h] = new T[size];
-        for (int w = 0; w < size; ++w) {
-            if(h == w){
-                D[h][w] = M[h][w];
-            }else{
 
-                D[h][w] = 0;
-            }
-        }
-    }
-    return D;
-}
 
-Matrix DMatrix(Matrix M){
+Matrix DMatrix(Matrix &M){
+    int sizeRow = M.sizeRow;
+    int sizeCol = M.sizeCol;
     double** D = 0;
-    D = new double*[M.sizeRow];
-    for (int h = 0; h < M.sizeRow; h++)
+    D = new double*[sizeRow];
+    for (int h = 0; h < sizeRow; h++)
     {
-        D[h] = new double[M.sizeCol];
-        for (int w = 0; w < M.sizeCol; ++w) {
+        D[h] = new double[sizeCol];
+        for (int w = 0; w < sizeCol; ++w) {
             if(h == w){
                 D[h][w] = M.Matrix[h][w];
             }else{
@@ -111,40 +89,22 @@ Matrix DMatrix(Matrix M){
     }
     Matrix out;
     out.Matrix = D;
-    out.sizeCol = M.sizeCol;
-    out.sizeRow = M.sizeRow;
+    out.sizeCol = sizeCol;
+    out.sizeRow = sizeRow;
     return out;
 }
 
 
-//przejsc na matrix
-template <typename T>
-T** LMatrixT(int size, T **M){
-    T** L = 0;
-    L = new T*[size];
-    for (int h = 0; h < size; h++)
-    {
-        L[h] = new T[size];
-        for (int w = 0; w < size; ++w) {
-            if(w < h and M[h][w] != 0){
-                L[h][w] = M[h][w] * (-1);
-            }else{
 
-                L[h][w] = 0;
-            }
-        }
-    }
-
-    return L;
-}
-
-Matrix LMatrix(Matrix M){
+Matrix LMatrix(Matrix &M){
+    int sizeRow = M.sizeRow;
+    int sizeCol = M.sizeCol;
     double** L = 0;
-    L = new double*[M.sizeRow];
-    for (int h = 0; h < M.sizeRow; h++)
+    L = new double*[sizeRow];
+    for (int h = 0; h < sizeRow; h++)
     {
-        L[h] = new double[M.sizeCol];
-        for (int w = 0; w < M.sizeCol; ++w) {
+        L[h] = new double[sizeCol];
+        for (int w = 0; w < sizeCol; ++w) {
             if(w < h and M.Matrix[h][w] != 0){
                 L[h][w] = M.Matrix[h][w] * (-1);
             }else{
@@ -155,18 +115,20 @@ Matrix LMatrix(Matrix M){
     }
     Matrix out;
     out.Matrix = L;
-    out.sizeCol = M.sizeCol;
-    out.sizeRow = M.sizeRow;
+    out.sizeCol = sizeCol;
+    out.sizeRow = sizeRow;
     return out;
 }
 
-Matrix UMatrix(Matrix M){
+Matrix UMatrix(Matrix &M){
+    int sizeRow = M.sizeRow;
+    int sizeCol = M.sizeCol;
     double ** U = 0;
-    U = new double*[M.sizeRow];
-    for (int h = 0; h < M.sizeRow; h++)
+    U = new double*[sizeRow];
+    for (int h = 0; h < sizeRow; h++)
     {
-        U[h] = new double[M.sizeCol];
-        for (int w = 0; w < M.sizeCol; ++w) {
+        U[h] = new double[sizeCol];
+        for (int w = 0; w < sizeCol; ++w) {
             if(w > h and M.Matrix[h][w] != 0){
                 U[h][w] = M.Matrix[h][w] * (-1);
             }else{
@@ -177,18 +139,9 @@ Matrix UMatrix(Matrix M){
     }
     Matrix out;
     out.Matrix = U;
-    out.sizeRow = M.sizeRow;
-    out.sizeCol = M.sizeCol;
+    out.sizeRow = sizeRow;
+    out.sizeCol = sizeCol;
     return out;
-}
-
-
-template <typename T>
-void displayVector(int size, T* V){
-    for (int i = 0; i < size; ++i) {
-        std::cout << std::setw(2) << std::right << V[i] << " ";
-    }
-    std::cout << std::endl;
 }
 
 
@@ -210,28 +163,34 @@ Matrix createMyMatrixNxN(int size, double a1, double a2, double a3){
     return out;
 
 }
-void createDiagonalNxN(Matrix M, double value = 1){
-    for (int row = 0; row < M.sizeRow; ++row) {
+void createDiagonalNxN(Matrix &M, double value = 1){
+    int sizeRow = M.sizeRow;
+
+    for (int row = 0; row < sizeRow; ++row) {
         M.Matrix[row][row] = value;
     }
 }
-Matrix copyMatrix(Matrix M){
-    Matrix out = createMatrix(M.sizeRow, M.sizeCol);
-    for (int row = 0; row < M.sizeRow; ++row) {
-        for (int col = 0; col < M.sizeCol; ++col) {
+Matrix copyMatrix(Matrix &M){
+    int sizeRow = M.sizeRow;
+    int sizeCol = M.sizeCol;
+    Matrix out = createMatrix(sizeRow, sizeCol);
+    for (int row = 0; row < sizeRow; ++row) {
+        for (int col = 0; col < sizeCol; ++col) {
             out.Matrix[row][col] = M.Matrix[row][col];
         }
     }
     return out;
 }
-Matrix inverseMatrix(Matrix A){
-    Matrix I = createMatrix(A.sizeRow, A.sizeCol);
+Matrix inverseMatrix(Matrix &A){
+    int sizeRow = A.sizeRow;
+    int sizeCol = A.sizeCol;
+    Matrix I = createMatrix(sizeRow, sizeCol);
     createDiagonalNxN(I);
     Matrix tmp = copyMatrix(A);
     double a, b; // współczynniki przy macierzach sprawdzone przed zmianamina wierszu
-    for (int row = 0; row < tmp.sizeRow; ++row) {
+    for (int row = 0; row < sizeRow; ++row) {
         a = tmp.Matrix[row][row];
-        for (int col = 0; col < tmp.sizeCol; ++col) {
+        for (int col = 0; col < sizeCol; ++col) {
             if(a== 0){
                 break;
             }else{
@@ -241,10 +200,10 @@ Matrix inverseMatrix(Matrix A){
                 I.Matrix[row][col] = tmp2;
             }
         }
-        for (int row2 = 0; row2 < I.sizeRow; ++row2) {
+        for (int row2 = 0; row2 < sizeRow; ++row2) {
             if(row2 != row){
                 b = tmp.Matrix[row2][row];
-                for (int col = 0; col < I.sizeCol; ++col) {
+                for (int col = 0; col < sizeCol; ++col) {
                     double tmp1 = tmp.Matrix[row2][col] - (tmp.Matrix[row][col] * b);
                     tmp.Matrix[row2][col] = tmp1;
                     double tmp2 = I.Matrix[row2][col] - (I.Matrix[row][col] * b);
@@ -253,17 +212,20 @@ Matrix inverseMatrix(Matrix A){
             }
         }
     }
-//    displayMatrixT(tmp.sizeCol, tmp.Matrix);
-//    displayMatrixT(tmp.sizeCol, I.Matrix);
+
     return I;
 }
 
-Matrix multiplyMatrix(Matrix M, Matrix X){
-    Matrix out = createMatrix(X.sizeRow, X.sizeCol);
-    for (int rowM = 0; rowM < M.sizeRow; ++rowM) {
-        for (int colX = 0; colX < X.sizeCol; ++colX) {
+Matrix multiplyMatrix(Matrix &M, Matrix &X){
+    int MsizeRow = M.sizeRow;
+    int MsizeCol = M.sizeCol;
+    int XsizeRow = X.sizeRow;
+    int XsizeCol = X.sizeCol;
+    Matrix out = createMatrix(XsizeRow, XsizeCol);
+    for (int rowM = 0; rowM < MsizeRow; ++rowM) {
+        for (int colX = 0; colX < XsizeCol; ++colX) {
             double sum = 0;
-            for (int i = 0; i < M.sizeCol; ++i) {
+            for (int i = 0; i < MsizeCol; ++i) {
                 double tmp = M.Matrix[rowM][i] * X.Matrix[i][colX];
                 sum += tmp;
             }
@@ -273,59 +235,7 @@ Matrix multiplyMatrix(Matrix M, Matrix X){
     return out;
 }
 
-template <typename  T>
-T** inversMatrixNxN(int size, T** A){
-    T** I = 0;
-    I = new T*[size];
-    T** tmp = 0;
-    tmp = new T*[size];
-    for (int h = 0; h < size; h++)
-    {
-        I[h] = new T[size];
-        tmp[h] = new T[size];
-        for (int w = 0; w < size; ++w) {
-            tmp[h][w] = A[h][w];
-            if(h==w){
-                I[h][w] = 1;
-            }else{
-                I[h][w] = 0;
-            }
-        }
-    }
-    displayMatrixT(size, tmp);
-    displayMatrixT(size, I);
-    for (int h = 0; h < size; h++) {
-        T a = tmp[h][h];
-        if(a == 0){
-            std::cout<< "mamy problem a = 0\n";
-            a = 1;
-        }
-        T a2 = I[h][h];
-        if(a2 == 0){
-            std::cout<< "mamy problem a2 = 0\n";
-            a2 = 1;
-        }
-        for (int w = h; w < size; ++w) {
-            tmp[h][w] /= a;
-            I[h][w] /= a;
-        }
-        for (int h2 = 0; h2 < size; ++h2) {
-            T b = tmp[h2][h];
-            T b2 = I[h2][h];
-            if (h != h2){
-                for (int w2 = 0; w2 < size; ++w2) {
-                    tmp[h2][w2] -= tmp[h][w2] * b;
-                    I[h2][w2] -= I[h][w2] * b;
-                }
-            }
-        }
-    }
 
-    displayMatrixT(size, tmp);
-    displayMatrixT(size, I);
-
-    return I;
-}
 
 Matrix createMyVectorN(int size, double f){
     Matrix out = createMatrix(size, 1);
@@ -345,17 +255,19 @@ Matrix createVectorN(int size, double value){
 }
 
 
-double norm(Matrix M){
+double norm(Matrix &M){
     double sum = 0;
-    for (int row = 0; row < M.sizeRow; ++row) {
-        for (int col = 0; col < M.sizeCol; ++col) {
+    int sizeRow = M.sizeRow;
+    int sizeCol = M.sizeCol;
+    for (int row = 0; row < sizeRow; ++row) {
+        for (int col = 0; col < sizeCol; ++col) {
             sum += M.Matrix[row][col] * M.Matrix[row][col];
         }
     }
     return sqrt(sum);
 }
 
-Result GaussS(Matrix A, Matrix b, double eps){
+Result GaussS(Matrix &A, Matrix &b, double eps){
     long long ms = std::chrono::duration_cast< std::chrono::milliseconds >(
             std::chrono::system_clock::now().time_since_epoch()).count();
     Matrix Xin = createMatrix(b.sizeRow, b.sizeCol);
@@ -404,7 +316,7 @@ Result GaussS(Matrix A, Matrix b, double eps){
     return result;
 }
 
-Result Jacobi(Matrix A, Matrix b, double eps) {
+Result Jacobi(Matrix &A, Matrix &b, double eps) {
     long long ms = std::chrono::duration_cast< std::chrono::milliseconds >(
             std::chrono::system_clock::now().time_since_epoch()).count();
     Matrix Xin = createMatrix(b.sizeRow, b.sizeCol);
@@ -444,7 +356,7 @@ Result Jacobi(Matrix A, Matrix b, double eps) {
 }
 
 
-Result Gaussa(Matrix A, Matrix B){
+Result Gaussa(Matrix &A, Matrix &B){
     long long ms = std::chrono::duration_cast< std::chrono::milliseconds >(
             std::chrono::system_clock::now().time_since_epoch()).count();
     Matrix I = copyMatrix(B);
@@ -492,44 +404,15 @@ Result Gaussa(Matrix A, Matrix B){
     result.M = I;
     result.iteration = 0;
 //    result.norm = norm(addMatrixNxN(multiplyMatrix(A, I), B, -1));
-    result.norm = norm(addMatrixNxN(multiplyMatrix(A, I), B, mConst::ODEJMOWANIE));
+    Matrix multiAandI = multiplyMatrix(A, I);
+    Matrix addMforNorm = addMatrixNxN(multiAandI, B, mConst::ODEJMOWANIE);
+    result.norm = norm(addMforNorm);
 //    displayMatrix(tmp);
 //    displayMatrix(I);
     return result;
 
 }
 
-Matrix invers2NxN(Matrix A){
-    Matrix out = copyMatrix(A);
-    double ratio, a;
-    int i, j, k, n;
-    n = A.sizeRow;
-    for(i = 0; i < n; i++){
-        for(j = n; j < 2*n; j++){
-            if(i==(j-n))
-                out.Matrix[i][j] = 1.0;
-            else
-                out.Matrix[i][j] = 0.0;
-        }
-    }
-    for(i = 0; i < n; i++){
-        for(j = 0; j < n; j++){
-            if(i!=j){
-                ratio = out.Matrix[j][i]/out.Matrix[i][i];
-                for(k = 0; k < 2*n; k++){
-                    out.Matrix[j][k] -= ratio * out.Matrix[i][k];
-                }
-            }
-        }
-    }
-    for(i = 0; i < n; i++){
-        a = out.Matrix[i][i];
-        for(j = 0; j < 2*n; j++){
-            out.Matrix[i][j] /= a;
-        }
-    }
-    return out;
-}
 
 int main() {
 //    std::cout.precision(4);
@@ -661,25 +544,29 @@ int main() {
     std::cout << ms;
 */
 
-//    Result R = Jacobi(A, b, mConst::eps);
+    Result R = Jacobi(A, b, mConst::eps);
     std::cout<<"----------------------------------------------------" <<std::endl;
-//    std::cout<<R.iteration<<"\n"<<R.duration<<"\n";
-//    displayMatrix(R.M);
-//    Result R2 = GaussS(A, b, mConst::eps);
+    std::cout<<R.iteration<<"\n"<<R.duration<<"\n";
+    displayMatrix(R.M);
+    Result R2 = GaussS(A, b, mConst::eps);
     std::cout<<"----------------------------------------------------" <<std::endl;
-//    std::cout << R2.iteration << "\n" << R2.duration << "\n";
-//    displayMatrix(R2.M);
+    std::cout << R2.iteration << "\n" << R2.duration << "\n";
+    displayMatrix(R2.M);
     Result R3 = Gaussa(A, b);
     std::cout<<"----------------------------------------------------" <<std::endl;
     std::cout << R3.iteration << "\n" << R3.duration << "\n";
-//    displayMatrix(R3.M);
+    displayMatrix(R3.M);
 //    ----------KONIEC TESTÓW--------------------Matrix AA = createMatrix(4, 4);
     Matrix BB = createMatrix(4, 1);
+    BB.Matrix[0][0] = 13.15;
+    BB.Matrix[1][0] = 49.84;
+    BB.Matrix[2][0] = -14.08;
+    BB.Matrix[3][0] = -46.51;
     Matrix AA = createMatrix(4,4);
-    double g4[4][4] = {10, -1, 2, 0,
-                       -1, 11, -1, 3,
-                       2, -1, 10, -1,
-                       0, 3, -1, 8};
+    double g4[4][4] = {1.2, 2.6, -0.1, 1.5,
+                       4.5, 9.8, -0.4, 5.7,
+                       0.1, -0.1, -0.3, -3.5,
+                       4.5, -5.2, 4.2, -3.4};
 
     double ** y4 = 0;
     y4 = new double*[4];
@@ -691,10 +578,9 @@ int main() {
         }
     }
     AA.Matrix = y4;
-//    displayMatrix(AA);
-//    Matrix odAA = inverseMatrix(AA);
-//    Matrix oddAA = invers2NxN(AA);
-//    displayMatrix(odAA);
-//    displayMatrix(oddAA);
+    Result R4 = Gaussa(AA, BB);
+    std::cout<<"----------------------------------------------------" <<std::endl;
+    std::cout << R4.iteration << "\n" << R4.duration << "\n";
+    displayMatrix(R4.M);
     return 0;
 }
